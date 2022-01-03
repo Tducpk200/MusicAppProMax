6c87017425005c7429be3ff6126ec2f7601d412d
TDuc <tienduc05102001@gmail.com>
2022-01-03 12:32:25 +0700

    add musicapppromax

---
+++ app/src/main/java/com/example/musicapppromax/Adapter/MusicAdapter.java
@@ -0,0 +1,94 @@
+package com.example.musicapppromax.Adapter;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaMetadataRetriever;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.musicapppromax.Activity.PlayerActivity;
+import com.example.musicapppromax.Files.MusicFiles;
+import com.example.musicapppromax.R;
+
+import java.util.ArrayList;
+
+public class MusicAdapter extends RecyclerView.Adapter<MusicAdapter.MyVieHolder> {
+
+    private Context mContext;
+    private ArrayList<MusicFiles> mFiles;
+
+    public MusicAdapter(Context mContext, ArrayList<MusicFiles> mFiles){
+        this.mFiles = mFiles;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public MyVieHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.music_item, viewGroup, false);
+        return new MyVieHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyVieHolder myVieHolder, @SuppressLint("RecyclerView") int i) {
+
+        myVieHolder.singer_name.setText(mFiles.get(i).getArtist());
+        myVieHolder.file_name.setText(mFiles.get(i).getTitle());
+        byte[] image = getAlbumArt(mFiles.get(i).getPath());
+        if(image != null){
+            Glide.with(mContext)
+                    .asBitmap()
+                    .load(image)
+                    .into(myVieHolder.album_art);
+        }
+        else {
+            Glide.with(mContext)
+                    .load(R.drawable.background_new_year_horenito)
+                    .into(myVieHolder.album_art);
+        }
+        myVieHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, PlayerActivity.class);
+                intent.putExtra("position", i);
+                mContext.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mFiles.size();
+    }
+
+    public class MyVieHolder extends RecyclerView.ViewHolder{
+
+
+        TextView file_name,singer_name;
+        ImageView album_art;
+
+        public MyVieHolder(@NonNull View itemView) {
+            super(itemView);
+            file_name = itemView.findViewById(R.id.txtSongName);
+            singer_name = itemView.findViewById(R.id.txtSingerName);
+            album_art = itemView.findViewById(R.id.imgSong);
+        }
+    }
+
+    private  byte[] getAlbumArt(String uri){
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        retriever.setDataSource(uri);
+        byte[] art = retriever.getEmbeddedPicture();
+        retriever.release();
+        return  art;
+    }
+
+}
